ab066e0a4eb53ca5757840c611d959eb6f1fa203
MaroM <maro.muszynski@gmail.com>
2019-03-14 16:43:10 +0100

    add repository for UnitOfMeasure

---
+++ src/main/java/com/marom/recipemongo/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.marom.recipemongo.repositories;
+
+import com.marom.recipemongo.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, String> {
+}
