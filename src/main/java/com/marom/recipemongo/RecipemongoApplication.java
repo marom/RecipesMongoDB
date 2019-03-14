4677580e84fd22bca33e55b18860df8419951447
MaroM <maro.muszynski@gmail.com>
2019-03-14 11:07:31 +0100

    initial commit

---
+++ src/main/java/com/marom/recipemongo/RecipemongoApplication.java
@@ -0,0 +1,13 @@
+package com.marom.recipemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipemongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipemongoApplication.class, args);
+    }
+
+}
