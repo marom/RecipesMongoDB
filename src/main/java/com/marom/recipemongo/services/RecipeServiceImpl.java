946917d587a330edb724b1f3bd302a6018d503f7
MaroM <maro.muszynski@gmail.com>
2019-03-15 18:36:57 +0100

    add service ant its implementation for Recipe

--- src/main/java/com/marom/recipemongo/services/RecipeServiceImpl.java
+++ src/main/java/com/marom/recipemongo/services/RecipeServiceImpl.java
@@ -1,10 +1,12 @@
 package com.marom.recipemongo.services;
 
 import com.marom.recipemongo.domain.Recipe;
+import com.marom.recipemongo.exceptions.NotFoundException;
 import com.marom.recipemongo.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -22,5 +24,16 @@ public class RecipeServiceImpl implements RecipeService {
         Set<Recipe> allRecipes = new HashSet<>();
         recipeRepository.findAll().forEach(allRecipes::add);
         return allRecipes;
+    }
+
+    @Override
+    public Recipe findById(String id) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+
+        if (!recipeOptional.isPresent()) {
+            throw new NotFoundException("Recipe Not Found for Recipe Id: " + id);
+        }
+        return recipeOptional.get();
     }
 }
