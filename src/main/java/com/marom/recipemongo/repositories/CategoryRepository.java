8b861f3add17e6ff145cd324d5843c1590fe0d38
MaroM <maro.muszynski@gmail.com>
2019-03-15 10:18:49 +0100

    add findByDescription method

--- src/main/java/com/marom/recipemongo/repositories/CategoryRepository.java
+++ src/main/java/com/marom/recipemongo/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.marom.recipemongo.repositories;
 import com.marom.recipemongo.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, String> {
+
+    Optional<Category> findByDescription(String description);
 }
