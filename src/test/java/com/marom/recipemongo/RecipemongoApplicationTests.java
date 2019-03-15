93b12096be7e367177851f95ed30de7c361b2717
MaroM <maro.muszynski@gmail.com>
2019-03-15 11:04:27 +0100

    fix failing and ignored test, context should be DataMongoTest

--- src/test/java/com/marom/recipemongo/RecipemongoApplicationTests.java
+++ src/test/java/com/marom/recipemongo/RecipemongoApplicationTests.java
@@ -1,16 +1,14 @@
 package com.marom.recipemongo;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataMongoTest
 public class RecipemongoApplicationTests {
 
-    @Ignore
     @Test
     public void contextLoads() {
     }
