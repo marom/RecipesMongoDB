60e3372a223e336a6cbef41c5c8d84af06a555c1
MaroM <maro.muszynski@gmail.com>
2019-03-15 10:19:06 +0100

    add findByDescription method

--- src/main/java/com/marom/recipemongo/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/marom/recipemongo/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.marom.recipemongo.repositories;
 import com.marom.recipemongo.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, String> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
