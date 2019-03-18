5ca6b5f78e51012e07b0cad71528de0fb890acfc
MaroM <maro.muszynski@gmail.com>
2019-03-18 10:20:05 +0100

    add exception handler on a controller level for a NotFoundException

--- src/main/java/com/marom/recipemongo/controllers/RecipeController.java
+++ src/main/java/com/marom/recipemongo/controllers/RecipeController.java
@@ -1,11 +1,16 @@
 package com.marom.recipemongo.controllers;
 
 import com.marom.recipemongo.domain.Recipe;
+import com.marom.recipemongo.exceptions.NotFoundException;
 import com.marom.recipemongo.services.RecipeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RecipeController {
@@ -23,5 +28,16 @@ public class RecipeController {
         Recipe recipe = recipeService.findById(recipeId);
         model.addAttribute("recipe", recipe);
         return "recipe/viewRecipe";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception){
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404");
+        modelAndView.addObject("exception", exception);
+        return modelAndView;
     }
 }
