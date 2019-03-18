90ed689fbd01e03cb7d1ff9abd4563441d5435c7
MaroM <maro.muszynski@gmail.com>
2019-03-18 18:53:50 +0100

    add test for service findById method

--- src/test/java/com/marom/recipemongo/services/RecipeServiceImplTest.java
+++ src/test/java/com/marom/recipemongo/services/RecipeServiceImplTest.java
@@ -1,18 +1,20 @@
 package com.marom.recipemongo.services;
 
 import com.marom.recipemongo.domain.Recipe;
+import com.marom.recipemongo.exceptions.NotFoundException;
 import com.marom.recipemongo.repositories.RecipeRepository;
 import org.junit.Before;
+
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
@@ -62,5 +64,30 @@ public class RecipeServiceImplTest {
 
         verify(recipeRepository, times(1)).findAll();
 
+    }
+
+    @Test
+    public void getRecipeByIdTest() {
+        Recipe recipe = new Recipe();
+        recipe.setId("abcd1234");
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyString())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById("abcd1234");
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyString());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getRecipeByIdTestNotFound() throws Exception {
+
+        Optional<Recipe> recipeOptional = Optional.empty();
+
+        when(recipeRepository.findById(anyString())).thenReturn(recipeOptional);
+
+        recipeService.findById("wrongId");
     }
 }
