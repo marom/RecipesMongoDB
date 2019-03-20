cf43eddb2624522f210cfccc6225a4c47282b024
MaroM <maro.muszynski@gmail.com>
2019-03-20 11:06:06 +0100

    add more tests to test for empty Categories and Ingredients when convert

--- src/test/java/com/marom/recipemongo/converters/RecipeToRecipeDtoTest.java
+++ src/test/java/com/marom/recipemongo/converters/RecipeToRecipeDtoTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import static org.hamcrest.Matchers.empty;
 import static org.junit.Assert.*;
 
 public class RecipeToRecipeDtoTest {
@@ -60,6 +61,84 @@ public class RecipeToRecipeDtoTest {
         assertEquals(recipeDto.getUrl(), recipe.getUrl());
         assertEquals(recipeDto.getDirections(), recipe.getDirections());
         assertEquals(recipeDto.getIngredients().iterator().next().getId(), ingredientConverter.convert(recipe.getIngredients().iterator().next()).getId());
+        assertArrayEquals(recipeDto.getImage(), recipe.getImage());
+        assertEquals(recipeDto.getDifficulty(), recipe.getDifficulty());
+        assertEquals(recipeDto.getNotes().getId(), notesConverter.convert(recipe.getNotes()).getId());
+        assertEquals(recipeDto.getCategories().iterator().next().getId(), categoryConverter.convert(recipe.getCategories().iterator().next()).getId());
+    }
+
+    @Test
+    public void whenConvertRecipeToDtoAndCategoriesAreEmptyThenCorrect() {
+
+        // given
+        final Recipe recipe = Recipe.builder()
+                .id("123ws")
+                .description("Recipe Description")
+                .prepTime(4)
+                .cookTime(65)
+                .servings(3)
+                .source("Recipe source")
+                .url("www.url.com")
+                .directions("one two three")
+                .ingredients(new HashSet<>(Arrays.asList(Ingredient.builder().id("34er").build())))
+                .image(ArrayUtils.toObject("Any String to bytes".getBytes()))
+                .difficulty(Difficulty.EASY)
+                .notes(Notes.builder().id("54rt").build())
+                .categories(new HashSet<>())
+                .build();
+
+        // when
+        final RecipeDto recipeDto = recipeToRecipeDto.convert(recipe);
+
+        // then
+        assertEquals(recipeDto.getId(), recipe.getId());
+        assertEquals(recipeDto.getDescription(), recipe.getDescription());
+        assertEquals(recipeDto.getPrepTime(), recipe.getPrepTime());
+        assertEquals(recipeDto.getCookTime(), recipe.getCookTime());
+        assertEquals(recipeDto.getServings(), recipe.getServings());
+        assertEquals(recipeDto.getSource(), recipe.getSource());
+        assertEquals(recipeDto.getUrl(), recipe.getUrl());
+        assertEquals(recipeDto.getDirections(), recipe.getDirections());
+        assertEquals(recipeDto.getIngredients().iterator().next().getId(), ingredientConverter.convert(recipe.getIngredients().iterator().next()).getId());
+        assertArrayEquals(recipeDto.getImage(), recipe.getImage());
+        assertEquals(recipeDto.getDifficulty(), recipe.getDifficulty());
+        assertEquals(recipeDto.getNotes().getId(), notesConverter.convert(recipe.getNotes()).getId());
+        assertThat(recipeDto.getCategories(), empty());
+    }
+
+    @Test
+    public void whenConvertRecipeToDtoAndIngredientsAreEmptyThenCorrect() {
+
+        // given
+        final Recipe recipe = Recipe.builder()
+                .id("123ws")
+                .description("Recipe Description")
+                .prepTime(4)
+                .cookTime(65)
+                .servings(3)
+                .source("Recipe source")
+                .url("www.url.com")
+                .directions("one two three")
+                .ingredients(new HashSet<>())
+                .image(ArrayUtils.toObject("Any String to bytes".getBytes()))
+                .difficulty(Difficulty.EASY)
+                .notes(Notes.builder().id("54rt").build())
+                .categories(new HashSet<>(Arrays.asList(Category.builder().id("cat1").build())))
+                .build();
+
+        // when
+        final RecipeDto recipeDto = recipeToRecipeDto.convert(recipe);
+
+        // then
+        assertEquals(recipeDto.getId(), recipe.getId());
+        assertEquals(recipeDto.getDescription(), recipe.getDescription());
+        assertEquals(recipeDto.getPrepTime(), recipe.getPrepTime());
+        assertEquals(recipeDto.getCookTime(), recipe.getCookTime());
+        assertEquals(recipeDto.getServings(), recipe.getServings());
+        assertEquals(recipeDto.getSource(), recipe.getSource());
+        assertEquals(recipeDto.getUrl(), recipe.getUrl());
+        assertEquals(recipeDto.getDirections(), recipe.getDirections());
+        assertThat(recipeDto.getIngredients(), empty());
         assertArrayEquals(recipeDto.getImage(), recipe.getImage());
         assertEquals(recipeDto.getDifficulty(), recipe.getDifficulty());
         assertEquals(recipeDto.getNotes().getId(), notesConverter.convert(recipe.getNotes()).getId());
