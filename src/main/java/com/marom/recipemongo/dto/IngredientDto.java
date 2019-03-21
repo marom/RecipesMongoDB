2749fb054e786ba091e5795fe15bf49a525a62f0
MaroM <maro.muszynski@gmail.com>
2019-03-21 08:53:19 +0100

    remove unused recipeId field

--- src/main/java/com/marom/recipemongo/dto/IngredientDto.java
+++ src/main/java/com/marom/recipemongo/dto/IngredientDto.java
@@ -10,7 +10,6 @@ import java.math.BigDecimal;
 public class IngredientDto {
 
     private String id;
-    private Long recipeId;
     private String description;
     private BigDecimal amount;
     private UnitOfMeasureDto uom;
