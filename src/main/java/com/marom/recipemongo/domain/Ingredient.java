e4a6ce3b5275613297c1ad67b8cf0843fe9355a7
MaroM <maro.muszynski@gmail.com>
2019-03-22 11:03:25 +0100

    generate id as MongoDb doesn't know it is id column

--- src/main/java/com/marom/recipemongo/domain/Ingredient.java
+++ src/main/java/com/marom/recipemongo/domain/Ingredient.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -16,7 +17,7 @@ import java.math.BigDecimal;
 public class Ingredient {
 
     @Id
-    private String id;
+    private String id = UUID.randomUUID().toString();
     private String description;
     private BigDecimal amount;
 
