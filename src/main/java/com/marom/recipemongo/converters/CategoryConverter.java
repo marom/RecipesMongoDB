ec5bb3d9ce6715c87911ab3a2c9ab7fe14d3a03b
MaroM <maro.muszynski@gmail.com>
2019-03-22 11:02:00 +0100

    add converter for category to convert String to category before update

---
+++ src/main/java/com/marom/recipemongo/converters/CategoryConverter.java
@@ -0,0 +1,34 @@
+package com.marom.recipemongo.converters;
+
+import com.marom.recipemongo.domain.Category;
+import com.marom.recipemongo.dto.CategoryDto;
+import com.marom.recipemongo.services.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CategoryConverter implements Converter<String, CategoryDto> {
+
+    private CategoryService categoryService;
+    private CategoryToCategoryDto categoryToCategoryDto;
+
+    public CategoryConverter(CategoryService categoryService, CategoryToCategoryDto categoryToCategoryDto) {
+        this.categoryService = categoryService;
+        this.categoryToCategoryDto = categoryToCategoryDto;
+    }
+
+    @Override
+    public CategoryDto convert(String categoryDtoId) {
+
+        log.info("trying to convert id: " + categoryDtoId + " to categoryDto");
+        if (categoryDtoId == null) {
+            return null;
+        }
+
+        Category category = categoryService.findById(categoryDtoId);
+
+        return categoryToCategoryDto.convert(category);
+    }
+}
