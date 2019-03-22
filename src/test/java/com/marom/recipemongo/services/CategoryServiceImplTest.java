927809bc22fe4c462ace1831716b34af189ae3da
MaroM <maro.muszynski@gmail.com>
2019-03-22 16:12:28 +0100

    add tests for category find by id

--- src/test/java/com/marom/recipemongo/services/CategoryServiceImplTest.java
+++ src/test/java/com/marom/recipemongo/services/CategoryServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.marom.recipemongo.services;
 
 import com.marom.recipemongo.domain.Category;
+import com.marom.recipemongo.exceptions.NotFoundException;
 import com.marom.recipemongo.repositories.CategoryRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,10 +9,13 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 public class CategoryServiceImplTest {
@@ -65,5 +69,33 @@ public class CategoryServiceImplTest {
 
         // then
         assertThat(allCategories, hasSize(0));
+    }
+
+    @Test
+    public void whenFindByIdCategoryThenCorrect() {
+
+        //given
+        Category categoryMexican = Category.builder().id("catM").description("Mexican").build();
+        Optional<Category> categoryOptional = Optional.of(categoryMexican);
+
+        //when
+        when(categoryRepository.findById(anyString())).thenReturn(categoryOptional);
+
+        Category category = categoryService.findById("catM");
+
+        //then
+        assertEquals(category.getId(), categoryMexican.getId());
+        assertEquals(category.getDescription(), categoryMexican.getDescription());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void whenCategoryNotFoundThenNotFoundExceptionIsThrown() {
+
+        //when
+        when(categoryRepository.findById(anyString())).thenReturn(Optional.empty());
+
+       categoryService.findById("cat1");
+
+       //then exception is thrown
     }
 }
