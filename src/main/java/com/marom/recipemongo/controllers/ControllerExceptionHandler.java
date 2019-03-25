28b39c51901d0103002dcbb738f841ee20daaa65
MaroM <maro.muszynski@gmail.com>
2019-03-25 17:51:27 +0100

    add Controller Advice exception handler for Number format exception

---
+++ src/main/java/com/marom/recipemongo/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.marom.recipemongo.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+@Slf4j
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleNumberFormat(Exception exception, Model model){
+
+        model.addAttribute("exception", exception);
+
+        return "400";
+    }
+}
